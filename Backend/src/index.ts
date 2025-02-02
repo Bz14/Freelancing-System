264d4ba76af638a9efab3ff21b3c50203ae8e1eb
Bz14 <bezueyerusalem@gmail.com>
2025-02-02 19:32:51 +0300

    Backend folder setup added.

---
+++ Backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
