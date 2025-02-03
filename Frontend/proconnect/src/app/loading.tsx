719f61834a7b8f72559832704ee2098ead59da5f
Bz14 <bezueyerusalem@gmail.com>
2025-02-03 18:50:47 +0300

    Loading page added.

--- Frontend/proconnect/src/app/loading.tsx
+++ Frontend/proconnect/src/app/loading.tsx
@@ -1,9 +1,15 @@
-const Loading = () => {
+import React from "react";
+import { FaSpinner } from "react-icons/fa";
+
+const LoadingPage = () => {
   return (
-    <div className="loading">
-      <div className="loading__spinner"></div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="flex items-center justify-center mb-4">
+        <FaSpinner className="animate-spin text-primary text-6xl" />
+      </div>
+      <h2 className="text-xl text-primary">Loading, please wait...</h2>
     </div>
   );
 };
 
-export default Loading;
+export default LoadingPage;
